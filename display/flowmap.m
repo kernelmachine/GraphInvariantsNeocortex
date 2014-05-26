60a512144be15ef3163ebc64bd2d108bbd4d5a81
ssgrn <sgrangan01@gmail.com>
2014-05-26 00:47:20 -0400

    Update flowmap.m

--- display/flowmap.m
+++ display/flowmap.m
@@ -5,22 +5,25 @@ function [stdang] = flowmap(coord,adjMatrix,pia)
     % adjMatrix: functional topology 
     % pia: 2 x 1 array, orientation of pia 
     %
-    % REQUIRES DOWNLOAD OF L2_DISTANCE
+    % REQUIRES DOWNLOAD OF L2_DISTANCE and ROTATION
     % (http://web.mit.edu/cocosci/isomap/code/L2_distance.m)
+    % (http://www.mathworks.com/matlabcentral/fileexchange/4257-coordinate-rotation/content/rotation.m)
     
     ang = angles(coord,pia);
     D = L2_distance(coord',coord',1);
     agl = ang(adjMatrix);
     [x,y] = pol2cart(agl,D(adjMatrix));
     stdang = circ_var(agl);
-    scatter(x,y,'r','filled');
+    
+    scatter(x,y,'r','filled');  % use compass(x,y) to display data in arrow format
+    
     axis off;
 end
 
 function [angleInRadians] = angles(coord,pia)
     % compute angle of cell coordinates relative to pia
     
-    as = vectorAngle(pia, [1 0]');
+    as = vectorAngle(pia, [0 1; 0 0]');
     rotxy = rotation(coord,[0 0], as,'radians');
     deltaX = bsxfun(@minus,rotxy(:,1),rotxy(:,1)');
     deltaY = bsxfun(@minus,rotxy(:,2),rotxy(:,2)');     
@@ -28,15 +31,11 @@ function [angleInRadians] = angles(coord,pia)
 end
 
 function [M] = vectorAngle(X1,X2)
-    % compute angle between coordinates of two vectors
+   % compute angle between coordinates of two lines
     
-    ns1 = sqrt(sum(X1 .* X1, 1));
-    ns2 = sqrt(sum(X2 .* X2, 1));
-    ns1(ns1 == 0) = 1;  
-    ns2(ns2 == 0) = 1;
-    M = bsxfun(@times, X1' * X2, 1 ./ ns1');
-    M = bsxfun(@times, M, 1 ./ ns2);
-    M = real(acos(M));
+   angle1 = atan2(X1(1,2) - X1(2,2), X1(1,1) - X1(2,1));
+   angle2 = atan2(X2(1,2) - X2(2,2), X2(1,1) - X2(2,1));
+   M = angle1 - angle2;
     
 end
     
